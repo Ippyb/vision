b5426b45568d55746ada2a6a700a2a2671d4a183
Ippyb <67248326+Ippyb@users.noreply.github.com>
2022-02-05 20:12:37 -0800

    Updated config file

--- Ipsita_vision/config.py
+++ Ipsita_vision/config.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -5,11 +7,11 @@ import numpy as np
 cameramtx = np.array([[662.3461673, 0, 293.51663222], [0, 664.22509824, 243.65759666],[  0, 0, 1]] )
 dist = np.array([[2.47936730e-01, -2.34092515e+00, 2.73073943e-03, -1.58373364e-03, 7.08333922e+00]])
 
-newcameramtx = np.array([[675.30944824, 0, 295.30676157], [0, 668.10913086, 243.85860464], [0, 0, 1]])
+#newcameramtx = np.array([[675.30944824, 0, 295.30676157], [0, 668.10913086, 243.85860464], [0, 0, 1]])
 
 #mine
-#newcameramtx= np.array([[1.12383716e+03, 0.00000000e+00, 5.33158579e+02], [0.00000000e+00, 1.06849597e+03, 4.85842118e+02],
-#                        [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+newcameramtx= np.array([[1.11528857e+03, 0.00000000e+00, 5.59369615e+02], [0.00000000e+00, 1.11225232e+03, 2.94226728e+02],
+                        [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
 
 roi = [] # TODO
@@ -28,11 +30,22 @@ nt_name = 'jetson'
 
 initial_camera_angle = 0
 
-tape_width = 12.7
+tape_width = 5
 
-tape_height = 5.08
+tape_height = 2
 
-tall_hub_height = 83.82 # just using counter height + half of box right now; cm
+tall_hub_height = 33.75 # just using counter height + half of box right now; cm
 
 short_hub_height = 0
 
+known_test_x = 19
+
+known_test_y = 10
+
+mount_angle = math.radians(28.389)
+
+sensor_width = 44.5 #mm
+
+sensor_height = 109 #mm
+
+sensor_depth = 39.3 #mm
