c47e76cbf37164b13396b4b7eca0d9e59600b9ac
Ippyb <67248326+Ippyb@users.noreply.github.com>
2022-02-13 15:18:46 -0800

    Update TEST.py

--- Ipsita_vision/TEST.py
+++ Ipsita_vision/TEST.py
@@ -56,17 +56,16 @@ r = config.tall_hub_radius
 
 
 # 3d points in real world space
-objpoints = [(r*math.cos(10*theta), r*math.sin(10*theta), 0),
-             (r*math.cos(11*theta), r*math.sin(11*theta), 0),
-             (r * math.cos(12 * theta), r * math.sin(12 * theta), 0),
-             (r * math.cos(13 * theta), r * math.sin(13 * theta), 0)
-             #((r * math.cos(14 * theta)), (r * math.sin(14 * theta)), 0)
-             #((r * math.cos(15 * theta)), (r * math.sin(15 * theta)), 0)
-]
+objpoints = np.array([[[r*math.cos(0), r*math.sin(0), 0]
+, [r*math.cos(theta), r*math.sin(theta), 0]
+, [r * math.cos(2 * theta), r * math.sin(2 * theta), 0]
+,[r * math.cos(3 * theta), r * math.sin(3 * theta), 0]
+]], np.float32)
 
 while True:
 
     ret, frame = cap.read()
+    print("in cam")
     h, w, _ = frame.shape
     cam_x = int((w / 2) - 0.5)
     cam_y = int((h / 2) - 0.5)
@@ -110,11 +109,11 @@ while True:
     # when using the draw rectangle thing use the center coordinates of the right-most and left-most tape contours
 
     # function that will ultimately help return the x-coordinate of a contour later
-    def leftToRight(array):
-        return array[1]
-
-    def leftToRight2(array):
-        return array[0]
+    # def leftToRight(array):
+    #     return array[1]
+    #
+    # def leftToRight2(array):
+    #     return array[0]
 
     if len(contours) != 0:
         for c in contours:
@@ -142,12 +141,14 @@ while True:
         imagepoints = np.array([imagepoints], np.float32)
 
         # Calibrate in real time
-        retval, rvecs, tvecs = cv2.solveP3P(objectPoints=objpoints, imagePoints=imagepoints, cameraMatrix=config.newcameramtx, distCoeffs=None, flags=cv2.SOLVEPNP_P3P)
-
-        #rvec to rotation matrix by axisangle to 3 by 3
-        rmatrix, _ = cv2.Rodrigues(np.array([rvecs[0][0][0],rvecs[0][1][0], rvecs[0][2][0]], np.float32))
+        retval, rvecs, tvecs = cv2.solveP3P(objectPoints=objpoints, imagePoints=imagepoints,
+                                            cameraMatrix=config.newcameramtx, distCoeffs=None, flags=cv2.SOLVEPNP_P3P)
+        # print(rvecs)
+        # print("tvecs: " + str(tvecs))
+        # rvec to rotation matrix by axisangle to 3 by 3
+        rmatrix, _ = cv2.Rodrigues(np.array([rvecs[0][0][0], rvecs[0][1][0], rvecs[0][2][0]], np.float32))
         transposed = rmatrix.T
-        tmatrix = np.array([tvecs[0][0][0],tvecs[0][1][0], tvecs[0][2][0]], np.float32).reshape(3,1)
+        tmatrix = np.array([tvecs[0][0][0], tvecs[0][1][0], tvecs[0][2][0]], np.float32).reshape(3, 1)
         real_cam_center = np.matmul(-transposed, tmatrix)
 
         print("real_cam_center: ", real_cam_center)
@@ -207,14 +208,11 @@ while True:
         # cv2.circle(frame, (int(xl), int(yl + (hl/2))), 7, (0, 255, 0), -1)
         # cv2.circle(frame, (int(xr + wr), int(yr + (hr/2))), 7, (0, 255, 0), -1)
 
-    cv2.imshow('Original', frame)
-    cv2.imshow('Mask', mask)
-
-    key = cv2.waitKey(1)
-    if key == 27:
-        print("l_h:" + str(l_h) + " " + "l_s:" + str(l_s) + " " + "l_v:" + str(l_v) + " " +
-                "u_h: " + str(u_h) + " " + "u_s:" + str(u_s) + " " + "u_v:" + " " + str(u_v))
-        break
+        cv2.imshow('Original', frame)
+        cv2.imshow('Mask', mask)
+        key = cv2.waitKey(100)
+        if key == 27:
+            break
 
 cap.release()
 cv2.destroyAllWindows()
