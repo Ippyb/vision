601422a5bcfb660e685f5a859e25447c14da5cc6
Ippyb <67248326+Ippyb@users.noreply.github.com>
2022-03-14 12:59:59 -0700

    with heights instead of contour area

--- Interpolation/main.py
+++ Interpolation/main.py
@@ -1,6 +1,6 @@
 # deltamath: https://www.desmos.com/calculator/z6dd2cz5y9
 # spreadsheet: https://docs.google.com/spreadsheets/d/1d8-onbCrB8yX4l5LnZqRzTZSysHMrqlJEVc6LUvenfE/edit?usp=sharing
-# equation of best fit: y = (411.164 * (math.e ** (-.00736918 * x))) + 79.8427
+# equation of best fit: y = (.0005 * (cx ** 2)) + (.0684372 * cx) + 79.8011
 
 import math
 import imutils
@@ -8,11 +8,12 @@ import numpy as np
 import cv2
 import glob
 
-images = glob.glob(
-    '/Users/ipsita/PycharmProjects/GRTVision 2021-2022/Ipsita_vision//Interpolation/Interpolation_images/83.5_images/*.png')
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_EXPOSURE,-13)
 
-for fname in images:
-    frame = cv2.imread(fname)
+while True:
+
+    ret, frame = cap.read()
     h, w, _ = frame.shape
     cam_x = int((w / 2) - 0.5)
     cam_y = int((h / 2) - 0.5)
@@ -28,24 +29,31 @@ for fname in images:
     mask = cv2.inRange(hsv, (0, 200, 17), (255, 255, 255))
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # find all lemon contours in mask
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
     if len(contours) != 0:
         c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])  # (testing)
+            cy = int(M["m01"] / M["m00"])
+        else:
+            cx, cy = 0, 0
+        center = (cx, cy)
         area = cv2.contourArea(c)
         x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        distance = (411.164 * (math.e ** (-.00736918 * area))) + 79.8427
-        print(distance)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
+        cv2.circle(frame, center, 1, (0, 255, 255), -1)
+        distance = y = (.0005 * (cx ** 2)) + (.0684372 * cx) + 79.8011
 
-
-        # print("Distance: 83.5 , contour area: " + str(area))
-        #print(str(area))
+        print(str(distance))
 
         cv2.imshow('Original', frame)
         # cv2.imshow('Mask', mask)
         key = cv2.waitKey(200)
         if key == 27:
             break
+
+cap.release()
+cv2.destroyAllWindows()
