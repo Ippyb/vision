1776972f640521c71a570189e53cfe4ff1db99f0
pinkbluesky <pinkbluesky@users.noreply.github.com>
2022-03-14 13:45:11 -0700

    exposure

--- Interpolation/main.py
+++ Interpolation/main.py
@@ -3,17 +3,43 @@
 # equation of best fit: y = (.0005 * (cx ** 2)) + (.0684372 * cx) + 79.8011
 
 import math
-import imutils
 import numpy as np
 import cv2
 import glob
 
-cap = cv2.VideoCapture(0)
+
+# Pulled from imutils package definition
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+                         "otherwise OpenCV changed their cv2.findContours return "
+                         "signature yet again. Refer to OpenCV's documentation "
+                         "in that case"))
+
+    # return the actual contours array
+    return cnts
+
+
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_EXPOSURE,-13)
 
 while True:
 
     ret, frame = cap.read()
+
     h, w, _ = frame.shape
     cam_x = int((w / 2) - 0.5)
     cam_y = int((h / 2) - 0.5)
@@ -30,7 +56,7 @@ while True:
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
+    contours = grab_contours(contours)
 
     if len(contours) != 0:
         c = max(contours, key=cv2.contourArea)
@@ -50,10 +76,12 @@ while True:
         print(str(distance))
 
         cv2.imshow('Original', frame)
-        # cv2.imshow('Mask', mask)
+        cv2.imshow('Mask', mask)
         key = cv2.waitKey(200)
         if key == 27:
             break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
