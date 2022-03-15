c0c8010cb1e2f8cffc15f8cad543282509e2c60c
Ippyb <67248326+Ippyb@users.noreply.github.com>
2022-03-15 16:11:43 -0700

    Update main.py

--- Interpolation/main.py
+++ Interpolation/main.py
@@ -1,45 +1,19 @@
-# deltamath: https://www.desmos.com/calculator/z6dd2cz5y9
+# desmos: https://www.desmos.com/calculator/ehu1yvja9z
 # spreadsheet: https://docs.google.com/spreadsheets/d/1d8-onbCrB8yX4l5LnZqRzTZSysHMrqlJEVc6LUvenfE/edit?usp=sharing
-# equation of best fit: y = (.0005 * (cx ** 2)) + (.0684372 * cx) + 79.8011
+# equation of best fit: y = (36.75131166 * (math.e ** (0.002864827 * cx))) + 18.65849
 
 import math
+import imutils
 import numpy as np
 import cv2
 import glob
 
-
-# Pulled from imutils package definition
-def grab_contours(cnts):
-    # if the length the contours tuple returned by cv2.findContours
-    # is '2' then we are using either OpenCV v2.4, v4-beta, or
-    # v4-official
-    if len(cnts) == 2:
-        cnts = cnts[0]
-
-    # if the length of the contours tuple is '3' then we are using
-    # either OpenCV v3, v4-pre, or v4-alpha
-    elif len(cnts) == 3:
-        cnts = cnts[1]
-
-    # otherwise OpenCV has changed their cv2.findContours return
-    # signature yet again and I have no idea WTH is going on
-    else:
-        raise Exception(("Contours tuple must have length 2 or 3, "
-                         "otherwise OpenCV changed their cv2.findContours return "
-                         "signature yet again. Refer to OpenCV's documentation "
-                         "in that case"))
-
-    # return the actual contours array
-    return cnts
-
-
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_EXPOSURE,-13)
 
 while True:
 
     ret, frame = cap.read()
-
     h, w, _ = frame.shape
     cam_x = int((w / 2) - 0.5)
     cam_y = int((h / 2) - 0.5)
@@ -56,7 +30,7 @@ while True:
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = grab_contours(contours)
+    contours = imutils.grab_contours(contours)
 
     if len(contours) != 0:
         c = max(contours, key=cv2.contourArea)
@@ -67,21 +41,20 @@ while True:
         else:
             cx, cy = 0, 0
         center = (cx, cy)
+        print(cx)
         area = cv2.contourArea(c)
         x, y, w, h = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
         cv2.circle(frame, center, 1, (0, 255, 255), -1)
-        distance = y = (.0005 * (cx ** 2)) + (.0684372 * cx) + 79.8011
+        distance = (36.75131166 * (math.e ** (0.002864827 * cx))) + 18.65849
 
         print(str(distance))
 
         cv2.imshow('Original', frame)
-        cv2.imshow('Mask', mask)
+        # cv2.imshow('Mask', mask)
         key = cv2.waitKey(200)
         if key == 27:
             break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
