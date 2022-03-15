23e2cbb71c9845929803812b54d5383e16ba92a1
Ippyb <67248326+Ippyb@users.noreply.github.com>
2022-03-15 16:12:17 -0700

    Update interpolationTesting.py

--- Interpolation/interpolationTesting.py
+++ Interpolation/interpolationTesting.py
@@ -1,15 +1,16 @@
-# deltamath: https://www.desmos.com/calculator/z6dd2cz5y9
+# desmos: https://www.desmos.com/calculator/ehu1yvja9z
 # spreadsheet: https://docs.google.com/spreadsheets/d/1d8-onbCrB8yX4l5LnZqRzTZSysHMrqlJEVc6LUvenfE/edit?usp=sharing
-# equation of best fit: y = (411.164 * (math.e ** (-.00736918 * x))) + 79.8427
+# equation of best fit: y = (36.75131166 * (math.e ** (0.002864827 * cx))) + 18.65849
 
 import math
 import imutils
 import numpy as np
 import cv2
 import glob
+import math
 
 images = glob.glob(
-    '/Users/ipsita/PycharmProjects/GRTVision 2021-2022/Ipsita_vision//Interpolation/Interpolation_images/212.25_images/*.png')
+    '/Users/ipsita/PycharmProjects/GRTVision 2021-2022/Ipsita_vision//Interpolation/Interpolation_images/updated/70_images/*.png')
 
 for fname in images:
     frame = cv2.imread(fname)
@@ -28,17 +29,27 @@ for fname in images:
     mask = cv2.inRange(hsv, (0, 200, 17), (255, 255, 255))
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # find all lemon contours in mask
     contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
     if len(contours) != 0:
         c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])  # (testing)
+            cy = int(M["m01"] / M["m00"])
+        else:
+            cx, cy = 0, 0
+        center = (cx, cy)
+        print (cx)
         area = cv2.contourArea(c)
         x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        # print("Distance: 83.5 , contour area: " + str(area))
-        print(str(area))
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
+        cv2.circle(frame, center, 1, (0, 255, 255), -1)
+        distance = (36.75131166 * (math.e ** (0.002864827 * cx))) + 18.65849
+
+        print(str(distance) + ", " + str(area))
+        #print(area)
 
         cv2.imshow('Original', frame)
         # cv2.imshow('Mask', mask)
